8c5ca80bdee72dd415b4dfe6f6ae420023e9d813
Selim Can Demirtaş <111413623+selimcandemirtas@users.noreply.github.com>
2022-08-23 10:07:12 +0300

    Update dataset_validation.py

--- recipes/dns_interspeech_2020/dataset_validation.py
+++ recipes/dns_interspeech_2020/dataset_validation.py
@@ -1,24 +1,66 @@
+import os
+from pathlib import Path
+
+import librosa
+
 from audio_zen.dataset.base_dataset import BaseDataset
+from audio_zen.acoustics.feature import load_wav
+from audio_zen.utils import basename
+
 
 class Dataset(BaseDataset):
-
     def __init__(
             self,
-            noisyWaves
+            dataset_directory,
+            sr,
     ):
+        """
+        Construct DNS validation set
 
+        synthetic/
+            with_reverb/
+                noisy/
+                clean_y/
+            no_reverb/
+                noisy/
+                clean_y/
+        """
         super(Dataset, self).__init__()
+        noisy_files_list = []
 
-        self.noisyWaves = noisyWaves
-        self.length = len(self.noisyWaves)
+        dataset_dir = Path(dataset_directory).expanduser().absolute()
+        noisy_files_list += librosa.util.find_files((dataset_dir / "noisy").as_posix())
 
+        self.length = len(noisy_files_list)
+        self.noisy_files_list = noisy_files_list
+        self.sr = sr
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, item):
-        
-        noisy = self.noisyWaves
-        clean = []
-        
-        return noisy, clean
+        """
+        use the absolute path of the noisy speech to find the corresponding clean speech.
+
+        Notes
+            with_reverb and no_reverb dirs have same-named files.
+            If we use `basename`, the problem will be raised (cover) in visualization.
+
+        Returns:
+            noisy: [waveform...], clean: [waveform...], type: [reverb|no_reverb] + name
+        """
+        noisy_file_path = self.noisy_files_list[item]
+        parent_dir = Path(noisy_file_path).parents[1].name
+        noisy_filename, _ = basename(noisy_file_path)
+
+        # Find the corresponding clean speech using "parent_dir" and "file_id"
+        #file_id = noisy_filename.split("_")[-1]
+        file_id = noisy_filename.split('_')[0]
+        clean_filename = file_id + '_c.WAV'
+
+        clean_file_path = noisy_file_path.replace(f"noisy/{noisy_filename}", f"clean/{clean_filename}")
+
+        noisy = load_wav(os.path.abspath(os.path.expanduser(noisy_file_path)), sr=self.sr)
+        clean = load_wav(os.path.abspath(os.path.expanduser(clean_file_path)), sr=self.sr)
+
+        return noisy, clean, noisy_filename
