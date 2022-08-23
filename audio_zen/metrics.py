028b10c2d48a7dccd08716547520ae03652557fd
Selim Can Demirtaş <111413623+selimcandemirtas@users.noreply.github.com>
2022-08-23 13:24:18 +0300

    Update metrics.py

--- audio_zen/metrics.py
+++ audio_zen/metrics.py
@@ -90,6 +90,18 @@ def STOI(ref, est, sr=16000):
 
 
 def WB_PESQ(ref, est, sr=16000):
+    print('REF')
+    print(f'Type: {type(ref)}')
+    print(f'Length: {len(ref)}')
+    print('Samples: ')
+    print(ref)
+    
+    print('EST')
+    print(f'Type: {type(est)}')
+    print(f'Length: {len(est)}')
+    print('Samples: ')
+    print(est)
+    
     return pesq(sr, ref, est, "wb")
 
 
